76793d977e1866ea5efd66d90044dc0757887dbd
jabed <jabedzaman2004@gmail.com>
2022-11-05 01:49:39 +0530

    initialize

---
+++ pages/_app.js
@@ -0,0 +1,27 @@
+import "../styles/globals.css";
+import Head from "next/head";
+import { ThemeProvider } from "next-themes";
+import { AnimatePresence } from "framer-motion";
+// import Script from "next/script";
+// import Header from "../components/Header";
+// import Footer from "../components/Footer";
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <ThemeProvider attribute="class" defaultTheme="light">
+        <Head>
+            <title>Lovely Guruji</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        </Head>
+        <AnimatePresence
+          exitBeforeEnter
+          initial={false}
+          onExitComplete={() => window.scrollTo(0, 0)}
+        >
+          <Component {...pageProps} />
+        </AnimatePresence>
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    mode:'jit',
+    darkMode: 'class',
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        fontFamily: {
+          poppins: ["Poppins", "sans-serif"],
+        },
+      },
+    },
+    plugins: [require('@tailwindcss/typography')],
+  };
+  
