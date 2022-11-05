33258338abf43c3f615c3806dacf78ca4e8db43f
jabed <jabedzaman2004@gmail.com>
2022-11-05 15:13:28 +0530

    add: heroicons && headless ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,16 @@
 module.exports = {
-    mode:'jit',
-    darkMode: 'class',
-    content: [
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {
-        fontFamily: {
-          poppins: ["Poppins", "sans-serif"],
-        },
+  mode: "jit",
+  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
       },
     },
-    plugins: [require('@tailwindcss/typography')],
-  };
-  
+  },
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+};
