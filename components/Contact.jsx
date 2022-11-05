1862132a00b8df4227d2a95ee43349bf220584b0
jabed <jabedzaman2004@gmail.com>
2022-11-05 17:02:58 +0530

    typo

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -13,7 +13,7 @@ export default function Example() {
           <div className="  py-16 px-4 sm:px-6 lg:col-span-2 lg:px-8 lg:py-24 xl:pr-12">
             <div className="max-w-lg mx-auto">
               <h2 className="text-2xl font-extrabold tracking-tight dark:text-gray-100 text-gray-900 sm:text-3xl">
-                Get in touch
+                Facing Doubts ??? Queries???
               </h2>
               <p className="mt-3 text-lg leading-6 text-gray-500">
                 Feel free to drop a message and we would get back to you ASAP
