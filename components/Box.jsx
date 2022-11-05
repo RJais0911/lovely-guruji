1f0017d322b9b0933af49388b6f1460a925b7131
jabed <jabedzaman2004@gmail.com>
2022-11-05 17:08:39 +0530

    make responsive

--- components/Box.jsx
+++ components/Box.jsx
@@ -4,30 +4,39 @@ import Layout from "./Layout";
 
 function box() {
   return (
-    <Layout>
-      <div className="bg-gray-100 dark:bg-gray-900">
+    <div className="bg-gray-100 dark:bg-gray-900">
+      <Layout>
         <div className="max-w-7xl mx-auto p-10 flex flex-row justify-between">
           <div className="flex flex-row space-x-3 items-center max-w-sm">
             <PlayIcon className="w-10 h-10 text-gray-900 dark:text-gray-100 rounded-full p-2 dark:bg-gray-800 bg-gray-200" />
-            <p className="font-ubuntu dark:text-gray-200 text-gray-800 ">
-              Best of the tutorials available
+            <p className="font-ubuntu dark:text-gray-200 text-gray-800 lg:text-sm text-xs ">
+              <span className="hidden lg:inline-block">
+                Best of the tutorials available
+              </span>
+              <span className="lg:hidden inline-block">Best tutorials</span>
             </p>
           </div>
           <div className="flex flex-row space-x-3 items-center max-w-sm">
             <StarIcon className="w-10 h-10 text-gray-900 dark:text-gray-100 rounded-full p-2 dark:bg-gray-800 bg-gray-200" />
-            <p className="font-ubuntu dark:text-gray-200 text-gray-800 ">
-              Best Content available on the internet
+            <p className="font-ubuntu dark:text-gray-200 text-gray-800 lg:text-sm text-xs ">
+              <span className="hidden lg:inline-block">
+                Best of the content available
+              </span>
+              <span className="lg:hidden inline-block">Best Content</span>
             </p>
           </div>
           <div className="flex flex-row space-x-3 items-center max-w-sm">
             <HandIcon className="w-10 h-10 text-gray-900 dark:text-gray-100 rounded-full p-2 dark:bg-gray-800 bg-gray-200" />
-            <p className="font-ubuntu dark:text-gray-200 text-gray-800 ">
-              Active contact support to clear doubts
+            <p className="font-ubuntu dark:text-gray-200 text-gray-800 lg:text-sm text-xs ">
+              <span className="hidden lg:inline-block">
+                Active contact support to solve doubts
+              </span>
+              <span className="lg:hidden inline-block">Active Support</span>
             </p>
           </div>
         </div>
-      </div>
-    </Layout>
+      </Layout>
+    </div>
   );
 }
 
