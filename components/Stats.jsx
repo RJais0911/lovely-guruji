5bb5d039e1f034f881808d7b675b92d9af15a7d7
jabed <jabedzaman2004@gmail.com>
2022-11-05 22:54:38 +0530

    fix: alignment

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -16,16 +16,16 @@ export default function Example() {
         <h3 className="lg:text-4xl text-lg font-semibold text-center leading-6 font-ubuntu  dark:text-gray-100 text-gray-900">
           Happy Learners
         </h3>
-        <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+        <dl className="mt-5 grid grid-cols-1 px-10 gap-5 sm:grid-cols-3">
           {stats.map((item) => (
             <div
               key={item.name}
-              className="px-4 py-5 bg-white dark:bg-black dark:shadow-gray-700 shadow rounded-lg overflow-hidden sm:p-6"
+              className="px-4 py-5 lg:text-left text-center bg-white dark:bg-black dark:shadow-gray-700 shadow rounded-lg overflow-hidden sm:p-6"
             >
               <dt className="text-sm font-medium text-gray-500 truncate">
                 {item.name}
               </dt>
-              <dd className="mt-1 text-3xl font-semibold dark:text-gray-100 text-gray-900"> 
+              <dd className="mt-1 lg:text-left text-center text-3xl font-semibold dark:text-gray-100 text-gray-900"> 
                 <CountTo from={0} digits={2} to={item.stat} speed={2000}>{fn}</CountTo>
               </dd>
             </div>
